fb05d90a9b95693b45ad71976c27d07bfee5edc6
Dat1Doge <zhangjiaweidaniel@gmail.com>
2024-12-07 15:50:58 -0800

    more

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import json
 
+with open("test.json","r") as f:
+    data = json.load(f)
+
+print(data["test"])
+print(type(data))
+with open("test.json","w") as f:
+    json.dump(data | {"AHHH":"GAHHHHH"},f,indent=4)
